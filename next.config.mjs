d03b43e09239cec72311d28f45d20a92924bbeda
Parth05d <darjiparth6850@gmail.com>
2026-03-16 17:40:41 +0530

    config file changed to deploy it in cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 1. Enables Static Export
+  output: 'export', 
+  
+  // 2. Disables the default Image Optimization API 
+  // (Required for static export unless using an external provider)
+  images: {
+    unoptimized: true,
+  },
+
+  // Optional: If you use trailing slashes in your URLs
+  // trailingSlash: true, 
+};
 
 export default nextConfig;
